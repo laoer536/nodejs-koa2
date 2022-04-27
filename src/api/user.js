dc052de4f63b86f1da92ac88cf99d33f4965085f
laoer536 <1547698569@qq.com>
2022-04-27 11:03:36 +0800

    优化user封装方式 看起来cool more

--- src/api/user.js
+++ src/api/user.js
@@ -3,28 +3,28 @@ const router = new Router({
   prefix: "/api",
 });
 
-router
-  .get("/", async (ctx) => {
-    console.log("查询参数", ctx.query);
-    ctx.body = "获取用户列表";
-  })
-  .get("/:id", async (ctx) => {
-    const { id } = ctx.params;
-    ctx.body = `获取id为${id}的用户`;
-  })
-  .post("/", async (ctx) => {
-    ctx.body = `创建用户`;
-  })
-  .put("/:id", async (ctx) => {
-    const { id } = ctx.params;
-    ctx.body = `修改id为${id}的用户`;
-  })
-  .del("/:id", async (ctx) => {
-    const { id } = ctx.params;
-    ctx.body = `删除id为${id}的用户`;
-  })
-  .all("/users/:id", async (ctx) => {
-    ctx.body = ctx.params;
-  });
+router.get("/", async (ctx) => {
+  console.log("查询参数", ctx.query);
+  ctx.body = "获取用户列表";
+});
+
+router.get("/:id", async (ctx) => {
+  const { id } = ctx.params;
+  ctx.body = `获取id为${id}的用户`;
+});
+
+router.post("/", async (ctx) => {
+  ctx.body = `创建用户`;
+});
+
+router.put("/:id", async (ctx) => {
+  const { id } = ctx.params;
+  ctx.body = `修改id为${id}的用户`;
+});
+
+router.del("/:id", async (ctx) => {
+  const { id } = ctx.params;
+  ctx.body = `删除id为${id}的用户`;
+});
 
 module.exports = router;
