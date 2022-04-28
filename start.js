aa87a6bf8deec0bd1540d9f94fa2cd0bf199cc9f
laoer536 <1547698569@qq.com>
2022-04-28 14:14:37 +0800

    删除多余文件

--- start.js
+++
@@ -1,7 +0,0 @@
-require("babel-register")({
-  plugins: ["babel-plugin-transform-es2015-modules-commonjs"],
-});
-
-require('dotenv').config({ path: '.env' })
-
-module.exports = require("./app.js");
