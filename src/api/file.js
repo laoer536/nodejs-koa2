d8c0448c0abe5262c7ef022995f3527890e20912
laoer536 <1547698569@qq.com>
2022-04-29 12:44:43 +0800

    文件上传下载部分完成

--- src/api/file.js
+++ src/api/file.js
@@ -1,10 +1,11 @@
 const Router = require("koa-router");
-const path = require("path");
+const sendfile = require("koa-sendfile");
 const fs = require("fs");
 const router = new Router({
   prefix: "/file",
 });
-router.post("/add", async (ctx) => {
+
+router.post("/upload", async (ctx) => {
   // 上传文件
   const { filepath, originalFilename } = ctx.request.files.file; // 获取上传文件
   const reader = fs.createReadStream(filepath);
@@ -13,16 +14,24 @@ router.post("/add", async (ctx) => {
   const upStream = fs.createWriteStream(filePath);
   // 可读流通过管道写入可写流
   reader.pipe(upStream);
-
-  console.log(process.env);
-
   ctx.response.body = {
-    rsCode: 200,
+    rsCode: 0,
     rsCause: "上传成功",
     data: {
-      fileUrl: `${process.env.INIT_CWD}/${filePath}`, //前面那个部分后期可以替换为其他可以访问的前缀 这里本地测试用
+      fileUrl: `${process.env.KOA_CORS_ORIGIN}/${filePath}`, //前面那个部分后期可以替换为其他可以访问的前缀 这里本地测试用
     },
   };
+});
+
+router.get("/download", async (ctx) => {
+  // 下载文件
+  console.log("ctx.request.query", ctx.request.query);
+  const { fileName } = ctx.request.query;
+  const path = `upload/${fileName}`;
+  console.log(path);
+  ctx.attachment(decodeURI(path)); //url编码 转换容易引起歧义的字符 例如中文字符
+  await sendfile(ctx, path); //前端页面需要对文件名进行decodeURIComponent解码 解码后能够拿到真正的名字
+  //另外 前端需要从返回体中获取文件名给a标签 从里面的headers的content-disposition中截取获得---》》》 res.headers["content-disposition"]
 });
 
 module.exports = router;
