1434a5cfe27b8dfd3e602531009daaf3aba42e51
laoer536 <1547698569@qq.com>
2022-05-02 15:29:01 +0800

    鉴权失败后提示修改

--- src/setting/error-handle.js
+++ src/setting/error-handle.js
@@ -3,7 +3,7 @@ const errHandle = async (ctx, next) =>
   next().catch((err) => {
     console.log("err信息", err);
     if (err.status === 401) {
-      ctx.fail("没有权限访问", 401); //处理jwt的token鉴权
+      ctx.fail("请先登录后访问", 401); //处理jwt的token鉴权
     } else {
       ctx.fail(err.message); //处理主要是接口调用产生的错误
     }
