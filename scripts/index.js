c43d1c8a5e0c243b16e6da10166241fa7d180468
laoer536 <1547698569@qq.com>
2023-02-08 19:51:50 +0800

    feat: 优化环境变量加载

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,11 @@
 const path = require('path')
+const argv = require('minimist')(process.argv.slice(2));
 require('babel-register')({
   plugins: ['babel-plugin-transform-es2015-modules-commonjs'],
 })
-
 require('dotenv').config({
-  path: path.resolve(process.cwd(), '.env.test'),
+  path: path.resolve(process.cwd(),`.env.${argv.mode}`),
 })
+console.log('当前环境',process.env)
 
 module.exports = require('../app.js')
