a0eb0f5db26a9b4ea104a4dc40c430434a1719fa
LJie <1547698569@qq.com>
2023-08-14 13:38:23 +0800

    perf: sendEmail

--- src/utils/nodemailer.ts
+++ src/utils/nodemailer.ts
@@ -1,5 +1,5 @@
 import nodemailer from 'nodemailer'
-import Mail from 'nodemailer/lib/mailer'
+import type Mail from 'nodemailer/lib/mailer'
 const transporter = nodemailer.createTransport({
   //node_modules/nodemailer/lib/well-known/services.json  查看相关的配置，如果使用qq邮箱，就查看qq邮箱的相关配置
   service: '163', //类型qq邮箱
@@ -13,8 +13,8 @@ const transporter = nodemailer.createTransport({
 })
 
 export function sendEmail(title: string, content: string, toMail: string) {
-  const mailOptions = {
-    from: `"laoer536" <laoer536@163.com>`, // 发送方
+  const mailOptions: Mail.Options = {
+    from: { name: 'koa-test', address: process.env.NODEMAILER_AUTH_EMAIL }, // 发送方
     to: toMail, //接收者邮箱，多个邮箱用逗号间隔
     subject: title, // 标题
     html: `<div>${content}</div>`, //页面内容
